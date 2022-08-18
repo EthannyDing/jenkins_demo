7a18d8be348dad25cbbbedd5c278f1d41b620740
Ethan Ding <43589728+EthannyDing@users.noreply.github.com>
2022-08-18 00:00:57 -0400

    delete one assert in test_4.py

--- tests/test_4.py
+++ tests/test_4.py
@@ -13,4 +13,4 @@ def test_get_mortality(test_df):
 
     assert test_df.shape[0] == 3, 'number of rows is not 3'
     assert test_df.shape[1] == 2, 'number of columns is not 2'
-    assert sum(test_df.shape) == 5, 'number of columns + row is not 5'
+
